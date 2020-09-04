a478c5e59c679dbbb303941f5fc426982c8f198d
Nikko Kowalow <69934240+2170644@users.noreply.github.com>
2020-09-04 13:58:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,15 @@ class Main {
   public static void main(String[] args) {
 
   }
+public static Shape2D[] getShapes() {
+  Triangle triangle = new Triangle(1,2,3);
+  Rectangle rectangle = new Rectangle(5,4);
+  Circle circle = new Circle(5.0);
+  Shape2D[] shapes = new Shape2D[3];
+  shapes[0] = triangle;
+  shapes[1] = rectangle;
+  shapes[2] = circle;
+  return shapes;
+}
 
-  public static Shape2D[] getShapes(){
-    Shape2D[] shapes = new Shape2D[3];
-    return shapes;
-  }
 }
