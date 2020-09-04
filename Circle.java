63b74f402ed551d141e6ec1e76cae23b38eaf6c2
Nikko Kowalow <69934240+2170644@users.noreply.github.com>
2020-09-04 13:58:29 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,7 +1,15 @@
-public abstract class Circle implements Shape2D {
+public class Circle implements Shape2D {
   double radius = 0.0;
 
   public Circle(double inputRadius) {
     this.radius = inputRadius;
   }
+
+  public double getArea() {
+    return Shape2D.PI * (this.radius * this.radius);
+  }
+
+  public double getPerimeter(){
+    return 2 * Shape2D.PI * this.radius;
+  }  
 }
